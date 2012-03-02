3262f9bf9aa8215fa5f0ab05ba1451d169495e64
James Landritsi <xlandisx@gmail.com>
2012-03-02 10:36:20 -0500

    added body for mos, and some small style changes

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 <!-- Bootstrap CSS Toolkit styles -->
 <link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap.min.css">
 <!-- Generic page styles -->
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="/fu/css/style.css">
 <!-- Bootstrap styles for responsive website layout, supporting different screen sizes -->
 <link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap-responsive.min.css">
 <!-- Bootstrap CSS fixes for IE6 -->
@@ -27,30 +27,8 @@
 <!-- Bootstrap Image Gallery styles -->
 <link rel="stylesheet" href="http://blueimp.github.com/Bootstrap-Image-Gallery/css/bootstrap-image-gallery.min.css">
 <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
-<link rel="stylesheet" href="css/jquery.fileupload-ui.css">
+<link rel="stylesheet" href="/fu/css/jquery.fileupload-ui.css">
 <!-- Shim to make HTML5 elements usable in older Internet Explorer versions -->
 <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
-<div class="navbar navbar-fixed-top">
-    <div class="navbar-inner">
-        <div class="container">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <a class="brand" href="http://ikon.org">IRIS Team</a>
-            <div class="nav-collapse">
-                <ul class="nav">
-                    <li class="active"><a href="#">Info</a></li>
-                    <li><a href="jim/">jim</a></li>
-                    <li><a href="charles/">charles</a></li>
-                    <li><a href="james/">james</a></li>
-                    <li><a href="erin/">erin</a></li>
-                    <li><a href="mos/">mos</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
---
+++ mos/body.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="page-header">
+		<h1>Joseph C. Philip</h1>
+		<h4 class="aka">a.k.a. "Mos MIF", "Jihad Joseph", "Token Brown", "The Man With the Apples"</h4>	
+	</div>
+	<img class="pic" src="../img/DSC_2611.jpg" alt="gansta" />
+	<br />	
+	<blockquote>Sup...</blockquote>
+	
+	<h3>You're going to be missed man</h3>
